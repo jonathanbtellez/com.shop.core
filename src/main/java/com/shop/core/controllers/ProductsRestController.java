7c1f68b5e1b25eb5742cc8dff5d797530cb1350a
Jonathan Tellez <jonathanbtellez@gmail.com>
2024-01-31 20:31:41 -0500

    consume Ecternal resources

--- src/main/java/com/shop/core/controllers/ProductsRestController.java
+++ src/main/java/com/shop/core/controllers/ProductsRestController.java
@@ -1,6 +1,7 @@
 package com.shop.core.controllers;
 
 import com.shop.core.configurations.ConfigurationParameters;
+import com.shop.core.models.Product;
 import com.shop.core.services.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -8,6 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -27,5 +32,15 @@ public class ProductsRestController {
 
         return  ResponseEntity.ok(productsService.getProducts());
     }
-    //page 150
+
+    @GetMapping("/fake-products")
+    public ResponseEntity<?> fakeProductsAPI(){
+         List<Product> products = new ArrayList<>(Arrays.asList(
+                new Product(1, "Product FAKE 1", 10.0, 10),
+                new Product(2, "Product FAKE 2", 20.0, 20),
+                new Product(3, "Product FAKE 3", 30.0, 30)
+        ));
+
+         return ResponseEntity.ok(products);
+    }
 }
---
+++ src/main/java/com/shop/core/services/ProductServicesAPIImpl.java
@@ -0,0 +1,33 @@
+package com.shop.core.services;
+
+import com.shop.core.models.Product;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+@ConditionalOnProperty(
+        value="products.strategy",
+        havingValue = "IN_API"
+)
+public class ProductServicesAPIImpl implements IProductService{
+    @Override
+    public List<Product> getProducts() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<List<Product>> response = restTemplate
+                .exchange("http://localhost:8080/shop/api/v1/products/fake-products",
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<List<Product>>() {}
+                );
+
+        List<Product> products = response.getBody();
+
+        return products;
+    }
+}
